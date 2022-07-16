e02f8862851dad1215b1b5eac99ac8dc15caf62f
playstargopal <gopal_12017079@nitkkr.ac.in>
2022-07-16 21:54:05 +0530

    commit

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+
     });
   
 
